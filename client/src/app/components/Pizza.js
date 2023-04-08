2111a4a8c09562cb4dc289b30d1a06af3420b926
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-04-08 14:35:08 +0300

    added

--- client/src/app/components/Pizza.js
+++ client/src/app/components/Pizza.js
@@ -1,19 +1,55 @@
 'use client';
+import { useState } from 'react';
 // image
 import Image from 'next/image';
 // link
 import Link from 'next/link';
+// modal
+import Modal from 'react-modal';
+
+// bind modal to #pizza
+Modal.setAppElement('#pizza');
+
+// modal styles
+const modalStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80vw',
+    maxWidth: '900px',
+  },
+};
 
 const Pizza = () => {
+  // modal state
+  const [modal, setModal] = useState(false);
+
+  // open modal
+  const openModal = () => {
+    setModal(true);
+  };
+
+  // close modal
+  const closeModal = () => {
+    setModal(false);
+  };
+
   return (
-    <div className='group py-2 px-4 xl:py-4 xl:px-2 rounded-xl'>
+    <div id='pizza' className='group py-2 px-4 xl:py-4 xl:px-2 rounded-xl'>
       <Link href={'/pizza/1'}>
         <Image
           width={270}
           height={270}
           src={'/vegetarian.webp'}
           alt=''
-          priority={'lazy'}
+          // priority={'lazy'}
           className='group-hover:translate-y-3 transition-all duration-300 mb-8'
         />
       </Link>
@@ -23,8 +59,8 @@ const Pizza = () => {
       </Link>
       {/* description */}
       <div className='text-sm text-[#5C6370] font-medium min-h-[60px] mb-6'>
-        Suncă, mozzarella, salam pepperoni, sos de rosii, rosii proaspete, feta,
-        ciuperci, condimente italiene mix
+        Ham, mozzarella, pepperoni salami, tomatoes sauce, fresh tomatoes, feta,
+        mushrooms, italian spices mix
       </div>
       {/* price & btn */}
       <div className='mb-6 flex items-center justify-between'>
@@ -34,19 +70,36 @@ const Pizza = () => {
         </div>
         {/* btn -> hidden (sm) - visible (lg) */}
         <button
-          onClick={() => console.log(20)}
+          onClick={openModal}
           className='hidden lg:flex btn btn-primary btn-sm'
         >
           Choose
         </button>
         {/* btn -> visible (sm) - hidden (lg) */}
         <button
-          onClick={() => console.log(20)}
+          onClick={openModal}
           className='btn btn-primary btn-sm lg:hidden px-5'
         >
           starts at $9.99
         </button>
       </div>
+      {/* modal */}
+      <Modal
+        isOpen={modal}
+        style={modalStyles}
+        onRequestClose={closeModal}
+        contentLabel='Pizza Modal'
+      >
+        <h2>Modal</h2>
+        <Image
+          width={270}
+          height={270}
+          src={'/vegetarian.webp'}
+          alt=''
+          // priority={1}
+          className='group-hover:translate-y-3 transition-all duration-300 mb-8'
+        />
+      </Modal>
     </div>
   );
 };
