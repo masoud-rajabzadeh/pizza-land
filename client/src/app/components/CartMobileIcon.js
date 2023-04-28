6137d7b414927587e7286bb85db7cd9aaf0d3c1a
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-04-28 13:12:43 +0300

    added

--- client/src/app/components/CartMobileIcon.js
+++ client/src/app/components/CartMobileIcon.js
@@ -1,18 +1,21 @@
 'use client';
 import { useContext } from 'react';
-import { IoBag } from 'react-icons/io5';
+import Image from 'next/image';
 // cart context
 import { CartContext } from '../context/CartContext';
+// react icons
+import { HiShoppingBag } from 'react-icons/hi';
 
 const CartMobileIcon = () => {
   const { isOpen, setIsOpen } = useContext(CartContext);
   return (
     <div
       onClick={() => setIsOpen(!isOpen)}
-      className='lg:hidden bg-black w-[70px] h-[70px] rounded-full flex justify-center items-center text-white cursor-pointer fixed right-[10%] bottom-[5%]'
+      className='lg:hidden bg-[#190b08] w-[72px] h-[72px] rounded-full flex justify-center items-center text-white cursor-pointer fixed right-[10%] bottom-[5%]'
     >
-      <IoBag className='text-3xl' />
-      <span className='absolute top-0 -right-1 bg-red-500 w-6 h-6 flex justify-center items-center rounded-full'>
+      <HiShoppingBag className='text-5xl' />
+      {/* <Image src={'bag.svg'} width={32} height={32} alt='' /> */}
+      <span className='absolute bottom-3 right-4 bg-primary text-white w-5 h-5 flex justify-center items-center rounded-full font-raleway text-base'>
         0
       </span>
     </div>
