6131f38a1a6cd096bfcfe912e19c393c0bfe5b1a
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-05-01 16:41:29 +0300

    added

--- client/src/app/components/CartDesktop.js
+++ client/src/app/components/CartDesktop.js
@@ -15,14 +15,15 @@ const CartDesktop = () => {
       } bg-tertiary fixed top-0 bottom-0 w-[500px] shadow-2xl hidden lg:flex flex-col transition-all duration-300`}
     >
       {/* top */}
-      <div className='w-full h-20 flex items-center justify-between px-10 text-white'>
-        <div>Shopping Bag(1)</div>
+      <div className='w-full h-20 flex items-center justify-between px-10 text-white bg-black/10'>
+        <div>Shopping Bag({cart.length})</div>
         {/* close icon */}
-        <div onClick={() => setIsOpen(false)} className='cursor-pointer'>
-          <IoCloseOutline className='text-3xl' />
+        <div onClick={() => setIsOpen(false)} className='cursor-pointer group'>
+          <IoCloseOutline className='text-3xl transition-all group-hover:rotate-180 duration-300' />
         </div>
       </div>
-      <div className='px-10 flex flex-col gap-y-4'>
+      {/* item list */}
+      <div className='px-10 flex flex-col gap-y-4 h-[70vh] overflow-y-scroll py-4'>
         {cart.map((pizza) => {
           console.log(pizza);
           return (
@@ -36,12 +37,14 @@ const CartDesktop = () => {
                   <div>qty: {pizza.amount}</div>
                   <div className='capitalize'>{pizza.crust} crust</div>
                   <div className='capitalize'>{pizza.size} size</div>
-                  <div>Price: {pizza.total}</div>
+                  <div>Price: {pizza.total * pizza.amount}</div>
                 </div>
               </div>
               {/* toppings */}
               <div className='flex flex-wrap items-center gap-3 p-4 border-b border-white/20'>
-                <div className=''>Toppings:</div>
+                <div>
+                  Toppings: {pizza.additionalTopping.length === 0 && 'None'}
+                </div>
                 {pizza.additionalTopping.map((topping) => {
                   return (
                     <div className='capitalize text-sm bg-secondary text-tertiary px-3 py-0.5 font-semibold rounded-full'>
