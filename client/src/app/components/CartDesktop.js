2243aef195cec8b0513061ab5cadaa82291a33c7
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-05-01 11:30:26 +0300

    added

--- client/src/app/components/CartDesktop.js
+++ client/src/app/components/CartDesktop.js
@@ -12,7 +12,7 @@ const CartDesktop = () => {
     <div
       className={`${
         isOpen ? 'left-0' : '-left-full'
-      } bg-tertiary fixed top-0 bottom-0 w-[400px] shadow-2xl hidden lg:flex flex-col transition-all duration-300`}
+      } bg-tertiary fixed top-0 bottom-0 w-[500px] shadow-2xl hidden lg:flex flex-col transition-all duration-300`}
     >
       {/* top */}
       <div className='w-full h-20 flex items-center justify-between px-10 text-white'>
@@ -22,17 +22,34 @@ const CartDesktop = () => {
           <IoCloseOutline className='text-3xl' />
         </div>
       </div>
-      <div className='px-10'>
+      <div className='px-10 flex flex-col gap-y-4'>
         {cart.map((pizza) => {
           console.log(pizza);
           return (
             <div className='text-white'>
-              <Image src={pizza.image} width={50} height={50} alt='' />
-              <div>{pizza.name}</div>
-              <div>qty: {pizza.amount}</div>
-              <div>{pizza.crust}</div>
-              <div>{pizza.size}</div>
-              <div>{pizza.total}</div>
+              <div className='flex gap-x-4'>
+                <div className='flex justify-start items-center'>
+                  <Image src={pizza.image} width={90} height={90} alt='' />
+                </div>
+                <div className='bg-green-200/20 flex-1'>
+                  <div className='text-xl capitalize'>Pizza {pizza.name}</div>
+                  <div>qty: {pizza.amount}</div>
+                  <div className='capitalize'>{pizza.crust} crust</div>
+                  <div className='capitalize'>{pizza.size} size</div>
+                  <div>Price: {pizza.total}</div>
+                </div>
+              </div>
+              {/* toppings */}
+              <div className='flex flex-wrap items-center gap-3 p-4 border-b border-white/20'>
+                <div className=''>Toppings:</div>
+                {pizza.additionalTopping.map((topping) => {
+                  return (
+                    <div className='capitalize text-sm bg-secondary text-tertiary px-3 py-0.5 font-semibold rounded-full'>
+                      {topping.name}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           );
         })}
