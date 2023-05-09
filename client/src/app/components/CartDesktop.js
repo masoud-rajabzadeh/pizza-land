d2d1a028c6a650ef7ddd9849e7fb3e1751857e0e
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-05-09 14:46:20 +0300

    added

--- client/src/app/components/CartDesktop.js
+++ client/src/app/components/CartDesktop.js
@@ -21,7 +21,7 @@ const CartDesktop = () => {
       <div
         className={`px-10 flex flex-col gap-y-4 h-[65vh] py-2 mr-4 mt-8 overflow-y-scroll scrollbar-thin ${
           cart.length >= 3 &&
-          'scrollbar-track-black/10 scrollbar-thumb-orange-500'
+          'scrollbar-track-black/10 scrollbar-thumb-secondary'
         }`}
       >
         {cart.map((pizza, index) => {
