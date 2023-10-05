e25f672b385b1f6219dcebb210fb6a219fac4192
m.zolfaghari <muhamad.zolfaghari@gmail.com>
2023-10-05 23:06:05 +0330

    Banner was added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import pizzas from '../data/pizzas.json';
+import Banner from './components/Banner';
 
 export default function Home() {
   return (
     <section>
+      <Banner />
       {pizzas.map((pizza) => (<div>{pizza.name}</div>))}
     </section>
   )
