ab896e85cc7d3bfa82d270e57e3c8c2b8f2fd086
Masoud <masood1371997@gmail.com>
2023-10-06 00:31:34 +0330

    CartBottom  wsa added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import pizzas from '../data/pizzas.json';
 import Banner from './components/Banner';
-
+import CartBottom from './components/CartBottom';
 export default function Home() {
   return (
     <section>
       <Banner />
+      <CartBottom/>
       {pizzas.map((pizza) => (<div>{pizza.name}</div>))}
     </section>
   )
