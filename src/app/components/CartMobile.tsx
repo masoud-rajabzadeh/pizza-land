4fb7b9f04ba9e4934779760d0c4d8c0dc4f84f38
Masoud <masood1371997@gmail.com>
2023-10-06 12:00:29 +0330

    CartMobile was added

---
+++ src/app/components/CartMobile.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { useContext } from 'react';
+// context
+import { CartContext } from '../context/CartContext';
+// components
+import CartTop from './CartTop';
+import CartBottom from './CartBottom';
+import CartItem from './CartItem';
+
+const CartMobile = () => {
+  const { cart, isOpen } = useContext(CartContext);
+  return (
+    <div
+      className={`${
+        isOpen ? 'bottom-0' : '-bottom-full'
+      } bg-white fixed w-full h-full left-0 z-20 transition-all duration-300 flex flex-col lg:hidden`}
+    >
+      {/* top */}
+      <CartTop />
+      {/* cart items */}
+      <div
+        className={`px-4 flex flex-col gap-y-4 py-2 mr-4 mt-8 h-[60vh] overflow-y-scroll scrollbar-thin scrollbar-thumb-secondary ${
+          cart.length >= 3
+            ? 'scrollbar-track-black/10'
+            : 'scrollbar-track-transparent'
+        }`}
+      >
+        {cart?.map((pizza, index) => {
+          return <CartItem pizza={pizza} key={index} />;
+        })}
+      </div>
+      {/* cart bottom */}
+      <CartBottom />
+    </div>
+  );
+};
+
+export default CartMobile;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import pizzas from '../data/pizzas.json';
 import Banner from './components/Banner';
 import CartBottom from './components/CartBottom';
 import CartDesktop from './components/CartDesktop'
+import CartItem from './components/CartItem';
+import CartMobile from './components/CartMobile';
 export default function Home() {
   return (
     <section>
       <Banner />
       <CartBottom />
       <CartDesktop />
+      <CartItem/>
+      <CartMobile/>
       {pizzas.map((pizza) => (<div>{pizza.name}</div>))}
     </section>
   )
